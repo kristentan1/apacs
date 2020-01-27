11191aec96108ec0b44698b6bf47a6eea6357789
Karun Sekhar <31802738+karsekh21@users.noreply.github.com>
2020-01-27 00:01:44 +0000

    Add files via upload

--- getsunsettime2.js
+++ getsunsettime2.js
@@ -1,44 +1,7 @@
-const Firebase = require('firebase');
 const axios = require('axios');
 
-let config = {
-    apiKey: "AIzaSyAnDPR1_QDrw5pKKxi17yzv9PJplzrf-Ww",
-    authDomain: "rnfirebase-d80d9.firebaseapp.com",
-    databaseURL: "https://rnfirebase-d80d9.firebaseio.com/",
-    projectId: "rnfirebase-d80d9",
-    storageBucket: "rnfirebase-d80d9.appspot.com",
-    messagingSenderId: "401684256760",
-    appId: "1:401684256760:web:6b7b4638c1c588fed4680d",
-    measurementId: "G-ZFVDNZS9R9"
-};
-let app = Firebase.initializeApp(config);
-const db = app.database();
-
-async function readFromDb(databaseArg) {
-    var ref = databaseArg.ref("/items");
-
-    let snapshotArray = [];
-
-    // Attach an asynchronous callback to read the data at our posts reference
-    ref.on("value", function (snapshot) {
-        let snapshotObj = snapshot.val();
-        snapshotArray = Object.values(snapshotObj);
-        console.log('++++++++++++++++++++++++++++++++');
-        console.log(snapshotObj);
-        console.log(typeof snapshot.val());
-        console.log(snapshotArray);
-        console.log(typeof snapshotArray);
-        console.log(snapshotArray[snapshotArray.length - 1].time);
-        console.log('++++++++++++++++++++++++++++++++');
-        return snapshotArray;
-    }, function (errorObject) {
-        return ("The read failed: " + errorObject.code);
-    });
-    return snapshotArray;
-}
-
 async function getTimes() {
-    const { data } = await axios.get('https://api.sunrise-sunset.org/json?lat=40.7440&lng=-74.0324&date=today');
+    const {data} = await axios.get('https://api.sunrise-sunset.org/json?lat=40.7440&lng=-74.0324&date=today');
     return data;
 }
 
@@ -53,40 +16,15 @@ async function convertSunsetTime() {
 }
 
 async function main() {
-    let overridden = false;
-    // Try again
-    var ref = db.ref("/items");
-    ref.on("value", function (snapshot) {
-        let snapshotObj = snapshot.val();
-        console.log(snapshotObj);
-        if (snapshotObj === null) {
-            return;
-        }
-        overridden = true;
-        snapshotArray = Object.values(snapshotObj);
-        return snapshotArray;
-    }, function (errorObject) {
-        return ("The read failed: " + errorObject.code);
-    });
-
+    // let date = new Date();
     const todaysSunsetTime = await convertSunsetTime();
-    let lightsOnTimeHour = todaysSunsetTime.hour - 5; // Convert from UTC to EST
+    let lightsOnTimeHour = todaysSunsetTime.hour + 12; // Convert from UTC to EST
     let lightsOnTimeMin = todaysSunsetTime.min - 20;
     if (lightsOnTimeMin < 0) {
         lightsOnTimeMin = 60 + lightsOnTimeMin;
         lightsOnTimeHour = lightsOnTimeHour - 1;
     }
-
-    // Check to see if sundowning prevention time has been manually overridden
-    if (overridden) {
-        snapshotNewestTime = snapshotArray[snapshotArray.length-1].time;
-        snapshotHourMinArr = snapshotNewestTime.split(':');
-        lightsOnTimeHour = parseInt(snapshotHourMinArr[0]);
-        lightsOnTimeMin = parseInt(snapshotHourMinArr[1]);
-        console.log('Lights-on time overridden.');
-        console.log('');
-    }
-
+    
     // todaysSunsetTime.hour = 12;
     // todaysSunsetTime.min = 40;
     console.log('Sunset hour and minute:');
@@ -95,11 +33,11 @@ async function main() {
 
     console.log();
     console.log('Lights on time hour and minute:');
-    // lightsOnTimeHour = 16;
-    // lightsOnTimeMin = 28;
+//     lightsOnTimeHour = 5;
+//     lightsOnTimeMin = 3;
     console.log(lightsOnTimeHour);
     console.log(lightsOnTimeMin);
-
+    console.log('~~~~~~~~~~');
     // console.log('Assume sunset time is ' + todaysSunsetTime.hour + ':' + todaysSunsetTime.min);
 
     while (true) {
@@ -108,7 +46,7 @@ async function main() {
         let date = new Date();
         if (date.getHours() === lightsOnTimeHour && date.getMinutes() === lightsOnTimeMin) {
             console.log('TRUE');
-            break; // The program should end after this. This is not the most updated code. (12/21/19)
+            break;
         }
     }
 
@@ -116,24 +54,24 @@ async function main() {
     console.log("Lights turning on!");
     // Send email
     // window.open('mailto:trigger@applet.ifttt.com');
-
-    var spawn = require('child_process').spawn,
-        py = spawn('python', ['turnonlights.py']),
-        data = [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        dataString = '';
+    
+   // var spawn = require('child_process').spawn,
+   // py = spawn('python', ['turnonlights.py']),
+   // data = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+   // dataString = '';
 
     // I think we don't need to do this because we don't need anything to happen when we return from the Python process.
-    py.stdout.on('data', function (data) {
+   // py.stdout.on('data', function(data){
         // dataString += data.toString();
-        console.log("");
-    });
-    py.stdout.on('end', function () {
-        console.log("");
-    });
-    py.stdin.write(JSON.stringify(data));
-    py.stdin.end();
+       // console.log("");
+   // });
+   // py.stdout.on('end', function(){
+   //     console.log("");
+   // });
+   // py.stdin.write(JSON.stringify(data));
+   // py.stdin.end();
 
-    console.log('Process complete');
+   // console.log('Process complete');
 }
 
 main();
