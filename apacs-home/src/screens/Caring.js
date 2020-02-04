3317b07c3e8d313b2b3215d31dae58009ffdfd63
mameer1 <33731371+mameer1@users.noreply.github.com>
2020-02-04 14:59:48 -0500

    Update caring.js to write to Firebase

--- apacs-home/src/screens/Caring.js
+++ apacs-home/src/screens/Caring.js
@@ -11,39 +11,63 @@ import {
 import { db } from '../config';
 
 let addItem = item => {
-    db.ref('/items').push({
-        name: item
+    db.ref('/reminders').push({
+        reminder: item
     });
 };
 
 export default class AddItem extends Component {
     state = {
-        name: ''
+        time: '',
+        text: ''
+
     };
 
-    handleChange = e => {
+    handleTimeChange = e => {
         this.setState({
-            name: e.nativeEvent.text
+            time: e.nativeEvent.text
         });
     };
 
+    handleReminderChange = e => {
+        this.setState({
+            text: e.nativeEvent.text
+        });
+    }
+
+
     handleSubmit = () => {
-        addItem(this.state.name);
-        Alert.alert('Item saved successfully');
+        let timeArr = this.state.time.split(':');
+        
+        console.log(timeArr);
+        console.log(parseInt(timeArr[0]));
+        if (this.state.time.length != 5) {
+            Alert.alert('Improper time format. Please enter a time in the format ##:##.');
+        } else if (timeArr.length != 2) {
+            Alert.alert('Improper time format. Please enter a time in the format ##:##.');
+        } else if ( isNaN(parseInt(timeArr[0])) || isNaN(parseInt(timeArr[1])) ) {
+            Alert.alert('Improper time format. Please enter a time in the format ##:##');
+        } else {
+            addItem(this.state);
+            //addItem(this.state.reminder)
+ //this.textInput.clear();
+            Alert.alert('Time set successfully');
+        }
     };
 
     render() {
         return (
             <View style={styles.main}>
-                <Text style={styles.title}>Sundowning Prevention</Text>
-                <Text>If you would like to override the default sundowning prevention time of 16:00, please enter a lights-on time in the format XX:XX below.</Text>
-                <TextInput style={styles.itemInput} onChange={this.handleChange} />
+                <Text style={styles.title}>Care Reminders</Text>
+                <Text style={{textAlign: 'center', marginBottom: 20}}>Set a daily reminder for yourself</Text>
+                <TextInput clearButtonMode="always" placeholder = "Time" style={styles.itemInput} onChange={this.handleTimeChange} />
+                <TextInput clearButtonMode="always" placeholder = "Reminder Text" style={styles.itemInput} onChange={this.handleReminderChange} />
                 <TouchableHighlight
                     style={styles.button}
                     underlayColor="white"
                     onPress={this.handleSubmit}
                 >
-                    <Text style={styles.buttonText}>Set Time</Text>
+                    <Text style={styles.buttonText}>Set Reminder</Text>
                 </TouchableHighlight>
             </View>
         );
@@ -56,7 +80,7 @@ const styles = StyleSheet.create({
         padding: 30,
         flexDirection: 'column',
         justifyContent: 'center',
-        backgroundColor: '#6565fc'
+        backgroundColor: 'red'
       },
       title: {
         marginBottom: 20,
@@ -66,12 +90,14 @@ const styles = StyleSheet.create({
       itemInput: {
         height: 50,
         padding: 4,
+        marginBottom: 5,
         marginRight: 5,
         fontSize: 23,
         borderWidth: 1,
         borderColor: 'white',
+        backgroundColor: 'white',
         borderRadius: 8,
-        color: 'white'
+        color: 'black'
       },
       buttonText: {
         fontSize: 18,
